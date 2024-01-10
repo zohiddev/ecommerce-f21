5f410394fa66ed97723512617fba5be2a39aa75c
7-zona <luvbrawl999@gmail.com>
2024-01-10 16:40:49 +0500

    add  products

---
+++ public/icons/MasterCard.jsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+const MasterCard = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={19}
+    height={12}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#EB001B"
+      fillRule="evenodd"
+      d="M7.09 5.853a5.79 5.79 0 0 1 2.17-4.531A5.65 5.65 0 0 0 5.715.074C2.559.074 0 2.661 0 5.853c0 3.19 2.559 5.778 5.715 5.778a5.65 5.65 0 0 0 3.545-1.248 5.79 5.79 0 0 1-2.17-4.53Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#FF5E00"
+      fillRule="evenodd"
+      d="M7.09 5.853a5.79 5.79 0 0 0 2.17 4.53 5.79 5.79 0 0 0 2.17-4.53 5.79 5.79 0 0 0-2.17-4.531 5.79 5.79 0 0 0-2.17 4.53Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#F79E1C"
+      fillRule="evenodd"
+      d="M12.805.074A5.65 5.65 0 0 0 9.26 1.322a5.79 5.79 0 0 1 2.17 4.53 5.79 5.79 0 0 1-2.17 4.531 5.65 5.65 0 0 0 3.545 1.248c3.156 0 5.715-2.587 5.715-5.778 0-3.192-2.56-5.779-5.715-5.779Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+export default MasterCard;
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import LockIcon from '../../public/icons/LockIcon';
 import MessageIcon from '../../public/icons/MessageIcon';
 import CarIcon from '../../public/icons/CarIcon'
+import ShopIcon from '../../public/icons/ShopIcon';
+import Productssuper from '../components/Productssuper';
 function ProductPage() {
   const products = [
     {
@@ -57,65 +59,153 @@ function ProductPage() {
       title: "Free delivery",
       subtitle: "Have you ever finally just"
     }
+  ];
+  const obj = [
+    {
+      id: 1,
+      price: "$99.50",
+      title: "GoPro HERO6 4K Action Camera - Black",
+      img: "./images/ipad.png",
+    },
+    {
+      id: 2,
+      price: "$99.50",
+      title: "GoPro HERO6 4K Action Camera - Black",
+      img: "./images/mobile.png",
+    },
+    {
+      id: 3,
+      price: "$99.50",
+      title: "GoPro HERO6 4K Action Camera - Black",
+      img: "./images/watch.png",
+    },
+    {
+      id: 4,
+      price: "$99.50",
+      title: "GoPro HERO6 4K Action Camera - Black",
+      img: "./images/laptop.png",
+    }
   ]
   return (
-    <section className="product">
+    <section className="products">
       <div className="container">
-        <h2 className='product-header__title'>
+        <h2 className='products-header__title'>
           My Cart (3)
         </h2>
-        <div className="product-row">
+        <div className="products-wrapper">
+        <div className="products-row">
           {
             products.map((item) => (
-              <div className="product-col" key={item.id}>
-                <div className="products">
-                  <div className="product-img">
+              <div className="products-col" key={item.id}>
+                <div className="productss">
+                  <div className="products-img">
                     <img src={item.img} alt="" />
                   </div>
-                  <div className="product-titles">
-                    <h3 className='product-desc'>{item.desc}</h3>
-                    <p className='product-title'>{item.title}</p>
-                    <p className='product-title'>{item.subtitle}</p>
-                    <div className="product-buttons">
-                      <button className='product-button'>Remove</button>
-                      <button className='product-button'>Save for later</button>
+                  <div className="products-titles">
+                    <h3 className='products-desc'>{item.desc}</h3>
+                    <p className='products-title'>{item.title}</p>
+                    <p className='products-title'>{item.subtitle}</p>
+                    <div className="products-buttons">
+                      <button className='products-button'>Remove</button>
+                      <button className='products-button'>Save for later</button>
                     </div>
                   </div>
                 </div>
-                <div className="product-option">
-                  <h3 className='product-price'>{item.price}</h3>
+                <div className="products-option">
+                  <h3 className='products-price'>{item.price}</h3>
                   <select name="select" id="" className='product-select'>
-                    <option value="select" className='product-option'>{item.option}</option>
-                    <option value="select" className='product-option'>{item.option1}</option>
-                    <option value="select" className='product-option'>{item.option2}</option>
+                    <option value="select" className='products-option'>{item.option}</option>
+                    <option value="select" className='products-option'>{item.option1}</option>
+                    <option value="select" className='products-option'>{item.option2}</option>
                   </select>
                 </div>
               </div>
             ))
 
           }
-          <div className="product-btns">
-            <button className='product-btn'>Bact to shop</button>
-            <button className='product-btn__one'>Remove all</button>
+          <div className="products-btns">
+            <button className='products-btn'>Bact to shop</button>
+            <button className='products-btn__one'>Remove all</button>
           </div>
         </div>
-        {
-          footer.map((item) => (
-            <div className="product-footer" key={item.id}>
-              <div className="product-footer__wrap">
-              <div className="product-footer__icon">
-                {item.icon}
+          <div className="products-item">
+            <div className="products-item__top">
+              <p>Have a coupon?</p>
+             <div className="products-item__buttons">
+             <input type="text" placeholder='Add coupon' />
+             <button className='products-item__button'>Apply</button>
+             </div>
+
+            </div>
+            <div className="products-item__bottom">
+              <div className="products-item__wrap">
+                <p className='products-item__title'>Subtotal:</p>
+                <p className='products-item__title'>$1403.97</p>
               </div>
-              <div className="product-footer__titles">
-                <h3 className='product-footer__title'>
-                  <h3 className='product-footer__title'>{item.title}</h3>
-                  <p className='product-footer__subtitle'>{item.subtitle}</p>
-                </h3>
+              <div className="products-item__wrap">
+                <p className='products-item__title'>Discount:</p>
+                <p className='products-item__title'>- $60.00</p>
               </div>
+              <div className="products-item__wrap">
+                <p className='products-item__title'>Tax::</p>
+                <p className='products-item__title'>+ $14.00</p>
+              </div>
+              <hr />
+              <div className="products-item__wrap">
+                <p className='products-item__title'><b>Total:</b></p>
+                <p className='products-item__title'><b>$1357.97</b></p>
+              </div>
+              <button>Checkout</button>
+
+              <div className="products-item__bottom__icons">
+                <>
+                <Misc
+                </>
               </div>
             </div>
-          ))
-        }
+          </div>
+        </div>
+        <div className="products-footer__wrapper">
+          {
+
+            footer.map((item) => (
+              <div className="products-footer" key={item.id}>
+                <div className="products-footer__wrap">
+                  <div className="products-footer__icon">
+                    {item.icon}
+                  </div>
+                  <div className="products-footer__titles">
+                    <h3 className='products-footer__title'>
+                      <h3 className='products-footer__title'>{item.title}</h3>
+                      <p className='products-footer__subtitle'>{item.subtitle}</p>
+                    </h3>
+                  </div>
+                </div>
+              </div>
+
+            ))
+          }
+        </div>
+        <div className="products-saved">
+          <h4 className="products-saved__title">Saved for later</h4>
+          <div className="products-saved__boxs">
+            {
+              obj.map( (items) => (
+                <div className="products-saved__box" key={items.id}>
+                  <div className="products-saved__box__img">
+                    <img src={items.img} alt="" />
+                  </div>
+                  <div className="products-saved__box__texts">
+                    <h5>{items.price}</h5>
+                    <p>{items.title}</p>
+                    <button className='products-saved__box__button'><span><ShopIcon /></span> <span>Move to cart</span></button>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <Productssuper />
       </div>
     </section>
   )
