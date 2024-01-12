ba3c3302d6a6f3bef492b6d29c5bd3becea6b378
gangster <mezarcisecret0305@gmail.com>
2024-01-12 16:02:47 +0500

    add detail sections

---
+++ src/components/sections/Productssuper.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Productssuper() {
+  return (
+    <div className="products-super__discount">
+            <div className="products-super__discount__right">
+              <h3>Super discount on more than 100 USD</h3>
+              <p>Have you ever finally just write dummy info</p>
+            </div>
+            <button >
+              Shop now
+            </button>
+          </div>
+  )
+}
+
+export default Productssuper;
