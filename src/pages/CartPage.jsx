9ba417500fb2327b40388573de650ab0ffa96ab9
7-zona <nurullayevmurodbek61@gmail.com>
2024-01-12 18:43:47 +0500

    CartPageStyle

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HeaderLink } from '../components'
 
 function CartPage() {
 
@@ -6,55 +7,55 @@ function CartPage() {
     {
         id :1,
         score:"7.5",
-        scImg: "../../public/images/str_1-3.png",
+        scImg: "../../public/images/str_1-3.svg",
         img: "../../public/images/image-1_9.png"
     },
     {
         id :2,
         score:"7.5",
-        scImg: "../../public/images/str_2.png",
+        scImg: "../../public/images/str_2.svg",
         img: "../../public/images/image_2-6.png"
     },
     {
         id :3,
         score:"7.5",
-        scImg: "../../public/images/str_1-3.png",
+        scImg: "../../public/images/str_1-3.svg",
         img: "../../public/images/image_3.png"
     },
     {
         id :4,
         score:"7.5",
-        scImg: "../../public/images/str_1-3.png",
+        scImg: "../../public/images/str_1-3.svg",
         img: "../../public/images/image_4.png"
     },
     {
         id :5,
         score:"7.5",
-        scImg: "../../public/images/str_2.png",
+        scImg: "../../public/images/str_2.svg",
         img: "../../public/images/image-5.png"
     },
     {
         id :6,
         score:"7.5",
-        scImg: "../../public/images/str_1-3.png",
+        scImg: "../../public/images/str_1-3.svg",
         img: "../../public/images/image_2-6.png"
     },
     {
         id :7,
         score:"7.5",
-        scImg: "../../public/images/str_1-3.png",
+        scImg: "../../public/images/str_1-3.svg",
         img: "../../public/images/image_7.png"
     },
     {
         id :8,
         score:"7.5",
-        scImg: "../../public/images/str_2.png",
+        scImg: "../../public/images/str_2.svg",
         img: "../../public/images/image-8.png"
     },
     {
         id :9,
         score:"7.5",
-        scImg: "../../public/images/str_1-3.png",
+        scImg: "../../public/images/str_1-3.svg",
         img: "../../public/images/image-1_9.png"
     }
   ]
@@ -152,8 +153,11 @@ function CartPage() {
   ]
 
   return (
-    <div className="CartPage-main">
-      <div className="container">
+    <div className="container">
+      <div className="CartPage__header">
+          <HeaderLink/>
+      </div>
+      <div className="CartPage">  
         <div className="CartPage_Left">
           <div className="CartPage_LeftCategories">
             <div className="CartPage_LeftCategorie">
@@ -208,7 +212,7 @@ function CartPage() {
             <div className="CartPage_LeftCategories_Head">
               <h3 className="CartPage_LeftCategories_HeadTytle">Price range</h3>
               <button className="CartPage_LeftCategories_HeadIcon">
-                <img src="../../public/images/bot_ic.svg" alt="" />
+                <img  src="../../public/images/bot_ic.svg" alt="" />
               </button>
             </div>
             </div>
@@ -254,14 +258,16 @@ function CartPage() {
                 </div>
                 <div className="CartPage_Right-header_top-buttons-select">
                   <span className="CartPage_Right-header_top-buttons-select-text">Featured</span>
-                  <input type="selectbox"  className='CartPage_Right-header_top-buttons-select-box'/>
+                  <button className="CartPage_LeftCategories_HeadIcon">
+                    <img src="../../public/images/bot_ic.svg" alt="" />
+                  </button>
                 </div>
                 <div className="CartPage_Right-header_top-buttons-btn">
                   <button className="CartPage_Right-header_top-buttons-btn_fr">
                     <img src="../../public/images/square.svg" alt="" />
                   </button>
-                  <button className="CartPage_Right-header_top-buttons-btn_fr">
-                  <img src="../../public/images/Menu.svg" alt="" />
+                  <button className="CartPage_Right-header_top-buttons-btn_sc">
+                    <img src="../../public/images/Menu.svg" alt="" />
                   </button>
                 </div>
               </div>
@@ -286,7 +292,7 @@ function CartPage() {
                   </div>
                   <div className="productCard-inf">
                     <div className="productCard-inf-cost">
-                      <h4 className="productCard-inf-costi_tytle">$99.50</h4>
+                      <h4 className="productCard-inf-cost_tytle">$99.50</h4>
                       <div className="productCard-inf-cost_score">
                         <div className="productCard-inf-cost_score-img">
                           <img src={product.scImg} alt="" />
@@ -307,7 +313,9 @@ function CartPage() {
           <div className="CartPage_Right-foot">
            <button className="CartPage_Right-footShow">
             <span className="CartPage_Right-footShow-text">Show 10</span>
-            <input type="selectbox" className="CartPage_Right-footShow" />
+            <button className="CartPage_LeftCategories_HeadIcon">
+                  <img src="../../public/images/bot_ic.svg" alt="" />
+            </button>
            </button>
            <div className="CartPage_Right-footBtn">
             <button className="CartPage_Right-footBtn_button"><img src="../../public/images/left-ic.svg" alt="" /></button>
